821ddca4985084ead8a04707cc4bf9c1658724a3
Maki0419-git <yoshinomaki90530@gmai.com>
2024-04-20 23:02:18 +0800

    feat(api): write api to get suggestion of city

---
+++ src/app/api/city/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest } from "next/server";
+import { CityCountyData } from "@/data/CityCountyData";
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("q") || "";
+  const cities = CityCountyData.reduce<string[]>((acc, data) => {
+    if (data.cityName.includes(query)) {
+      acc.push(data.cityName);
+    }
+    return acc;
+  }, []);
+  return Response.json({ cities });
+}
