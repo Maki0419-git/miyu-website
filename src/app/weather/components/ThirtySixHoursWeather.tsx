a2b5374e9cb5a41a3efabd26207e2f7e6a7b2d4f
Maki0419-git <yoshinomaki90530@gmai.com>
2024-04-25 23:18:08 +0800

    feat(weather): implement 36hours api and adjust layout

--- src/app/weather/components/ThirtySixHoursWeather.tsx
+++ src/app/weather/components/ThirtySixHoursWeather.tsx
@@ -1,13 +1,20 @@
 import errorHandler from "@/utils/errorHandler";
 import { getWeatherApiEndpoint } from "../utils/getWeatherApiEndpoint";
 import { styled } from "@pigment-css/react";
-import Image from "next/image";
+import { WeatherAPIResponse } from "../types";
+import { WEATHER_ICON } from "./WeatherIcon";
+import { getWeatherType } from "../utils/getWeatherType";
+import { WEATHER_CODE } from "../constant";
+import Humidity from "../../../assets/weather/humidity.svg";
+import dayjs from "dayjs";
 
-async function getThirtySixHoursWeather() {
+async function getThirtySixHoursWeather(): Promise<
+  WeatherAPIResponse<"THIRTY_SIX_HOURS_WEATHER">
+> {
   try {
     const endpoint = getWeatherApiEndpoint("THIRTY_SIX_HOURS_WEATHER");
     const response = await fetch(
-      `${endpoint}?locationName=新北市&Authorization=${process.env.WEATHER_API_KEY}`,
+      `${endpoint}?locationName=臺北市&Authorization=${process.env.WEATHER_API_KEY}`,
       { cache: "no-store" }
     );
 
@@ -35,34 +42,40 @@ const Container = styled("div")({
 
 const WeatherCard = styled("div")({
   position: "relative",
-  padding: "20px",
+  padding: "30px 20px",
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
   border: "1px solid yellow",
   borderRadius: "10px",
   backdropFilter: "blur(5px)",
-  minWidth: "200px",
+  minWidth: "250px",
+  maxWidth: "300px",
   width: "30%",
   height: "60%",
-  h3: { fontWeight: "normal", color: "white" },
-  img: {
+  "h2,h3,h4": { fontWeight: "normal", color: "white" },
+  svg: {
+    width: "100px",
+    height: "100px",
     margin: "10px 0 20px 0",
   },
 });
 
 const MinMaxTemperature = styled("div")({
+  margin: "10px 0",
   display: "flex",
-  flexDirection: "column",
-  gap: "10px",
+  alignItems: "center",
+  gap: "5px",
+  h3: {
+    color: "#dbdada",
+  },
 });
 
-const Temperature = styled("div")({
+const RainPossibility = styled("div")({
   position: "absolute",
   bottom: "0",
   left: "0",
   display: "flex",
-  flexDirection: "column",
   alignItems: "center",
   justifyContent: "center",
   width: "100%",
@@ -70,32 +83,99 @@ const Temperature = styled("div")({
   gap: "5px",
   backgroundColor: "rgba(0, 0, 0, 0.3)",
   borderRadius: "0 0 10px 10px",
+  svg: {
+    width: "60px",
+    height: "60px",
+  },
 });
 
-export default async function ThirtySixHoursWeather() {
-  // const thirtySixHoursWeatherData = await getThirtySixHoursWeather();
+type WeatherCard = {
+  startTime: string;
+  weatherElement: {
+    Wx: {
+      description: string;
+      code: number;
+    };
+    PoP: number;
+    MinT: number;
+    MaxT: number;
+  };
+};
 
-  // console.log(thirtySixHoursWeatherData);
+export default async function ThirtySixHoursWeather() {
+  const thirtySixHoursWeatherData = await getThirtySixHoursWeather();
+  const weatherElement =
+    thirtySixHoursWeatherData.records.location[0].weatherElement;
+  const weatherCardList: WeatherCard[] = weatherElement.reduce(
+    (acc, current) => {
+      const { elementName, time } = current;
+      if (elementName === "CI") return acc;
+      time.forEach((time, index) => {
+        if (!acc[index])
+          acc[index] = {
+            startTime: time.startTime,
+            weatherElement: {},
+          } as WeatherCard;
+        switch (elementName) {
+          case "Wx":
+            acc[index].weatherElement.Wx = {
+              description: time.parameter.parameterName,
+              code: Number(time.parameter.parameterValue as string),
+            };
+            break;
+          case "PoP":
+            acc[index].weatherElement.PoP = Number(
+              time.parameter.parameterName
+            );
+            break;
+          case "MinT":
+            acc[index].weatherElement.MinT = Number(
+              time.parameter.parameterName
+            );
+            break;
+          case "MaxT":
+            acc[index].weatherElement.MaxT = Number(
+              time.parameter.parameterName
+            );
+            break;
+        }
+      });
+
+      return acc;
+    },
+    [] as WeatherCard[]
+  );
+
+  console.log(thirtySixHoursWeatherData);
+  console.log({ weatherCardList });
 
   return (
     <Container>
-      {Array.from({ length: 3 }).map((_, index) => (
+      {weatherCardList.map((weatherCard, index) => (
         <WeatherCard key={index}>
-          <h3>Now</h3>
-          <Image
-            src="https://picsum.photos/200"
-            alt="weather icon"
-            width={90}
-            height={90}
-          />
+          <h3>
+            {dayjs(weatherCard.startTime).valueOf() < dayjs().valueOf()
+              ? "Now"
+              : dayjs(weatherCard.startTime).format("hh:mm A")}
+          </h3>
+          {
+            WEATHER_ICON.day[
+              getWeatherType(WEATHER_CODE, weatherCard.weatherElement.Wx.code)
+            ]
+          }
+          <h4>{weatherCard.weatherElement.Wx.description}</h4>
           <MinMaxTemperature>
-            <h3>25°C</h3>
-            <h3>20°C</h3>
+            <h3>{weatherCard.weatherElement.MinT}</h3>
+            <h3>/</h3>
+            <h2>{weatherCard.weatherElement.MaxT}°C</h2>
           </MinMaxTemperature>
-          <Temperature>
-            <h3>Feel Temperature</h3>
-            <h3>22°C</h3>
-          </Temperature>
+          <RainPossibility>
+            <Humidity />
+            <div>
+              <h2>{weatherCard.weatherElement.PoP}%</h2>
+              <h4>降雨機率</h4>
+            </div>
+          </RainPossibility>
         </WeatherCard>
       ))}
     </Container>
