0d75e712b01dfa3fdafaf50797fc00384b9642ad
Maki0419-git <yoshinomaki90530@gmai.com>
2024-04-27 22:57:23 +0800

    feat(middleware): parsed param `city` to chinese first to use `searchParams` more conveniently

---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const city = req.nextUrl.searchParams.get("city");
+  if (city === null) return NextResponse.next();
+  req.nextUrl.searchParams.set("city", decodeURIComponent(city));
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/weather/:path*",
+};
