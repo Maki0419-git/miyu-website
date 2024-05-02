d40b6f8704dc0412c1a6bce8e6355f4927e5c81f
Maki0419-git <yoshinomaki90530@gmai.com>
2024-05-02 15:27:17 +0800

    feat(utils): Add LocalStorage utility class for managing local storage data

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,24 @@
+export class LocalStorage<T> {
+  key: string;
+  defaultValue: T;
+
+  constructor(key: string, defaultValue: T) {
+    this.key = key;
+    this.defaultValue = defaultValue;
+  }
+
+  get() {
+    if (typeof window === "undefined") {
+      return this.defaultValue;
+    }
+    const storage = localStorage.getItem(this.key);
+    if (!storage) {
+      localStorage.setItem(this.key, JSON.stringify(this.defaultValue));
+      return this.defaultValue;
+    }
+    return JSON.parse(storage) as T;
+  }
+  set(value: T) {
+    localStorage.setItem(this.key, JSON.stringify(value));
+  }
+}
