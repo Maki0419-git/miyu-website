5318a86c67642459dbef9fcacda4d69b45a5ee7f
Maki0419-git <yoshinomaki90530@gmai.com>
2024-05-02 15:27:31 +0800

    feat(utils): Add LRUArray utility function to manage least recently used array in getLRUArray.ts

---
+++ src/utils/getLRUArray.ts
@@ -0,0 +1,13 @@
+export const LRUArray = <T>(array: T[], value: T, maxLength: number) => {
+  const index = array.indexOf(value);
+  if (index !== -1) {
+    array.splice(index, 1);
+  }
+  if (array.length >= maxLength) {
+    array.pop();
+  }
+
+  array.unshift(value);
+
+  return array;
+};
