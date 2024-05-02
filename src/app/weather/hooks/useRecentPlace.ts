68688cd0a203157abed942d70776bdb98a5cfddd
Maki0419-git <yoshinomaki90530@gmai.com>
2024-05-02 15:28:54 +0800

    feat(weather): Add useRecentPlace hook to manage recent places in weather app by local storage

---
+++ src/app/weather/hooks/useRecentPlace.ts
@@ -0,0 +1,28 @@
+"use client";
+
+import { LRUArray } from "@/utils/getLRUArray";
+import { LocalStorage } from "@/utils/localStorage";
+import { useEffect, useState } from "react";
+
+type RecentPlace = string[];
+const recentPlaceStorage = new LocalStorage<RecentPlace>("recentPlace", [
+  "臺中市",
+  "高雄市",
+  "宜蘭縣",
+]);
+
+export default function useRecentPlace() {
+  const [recentPlace, setRecentPlace] = useState<RecentPlace>([]);
+
+  const handlePlaceChange = (place: string) => {
+    const newStorage = LRUArray(recentPlace, place, 3);
+    setRecentPlace(newStorage);
+    recentPlaceStorage.set(newStorage);
+  };
+
+  useEffect(() => {
+    setRecentPlace(recentPlaceStorage.get());
+  }, []);
+
+  return { recentPlace, handlePlaceChange };
+}
