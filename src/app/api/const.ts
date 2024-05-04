9ddeb47bcd24b0f251c6b3b0fc4f8eb06469a52a
Maki0419-git <yoshinomaki90530@gmai.com>
2024-05-04 23:20:21 +0800

    feat(weather/api): use const  to define endpoint

---
+++ src/app/api/const.ts
@@ -0,0 +1,1 @@
+export const DOMAIN = process.env.NEXT_PUBLIC_PROD_DOMAIN || process.env.DEV_DOMAIN;
--- src/app/weather/hooks/useCity.ts
+++ src/app/weather/hooks/useCity.ts
@@ -1,26 +1,24 @@
 "use client";
 import useSWR from "swr";
 import { CityResponseType } from "../types";
+import { DOMAIN } from "@/app/api/const";
 
 export default function useCity(query: string) {
-  const { data, error, isLoading } = useSWR<CityResponseType, Error>(
-    query !== "" ? ["api/city", query] : null,
-    ([url, query]) =>
-      fetch(`http://localhost:3000/${url}?q=${query}`).then((res) =>
-        res.json()
-      ),
-    {
-      revalidateIfStale: false,
-      revalidateOnFocus: false,
-      revalidateOnReconnect: false,
-    }
-  );
+	const { data, error, isLoading } = useSWR<CityResponseType, Error>(
+		query !== "" ? ["api/city", query] : null,
+		([url, query]) => fetch(`${DOMAIN}${url}?q=${query}`).then((res) => res.json()),
+		{
+			revalidateIfStale: false,
+			revalidateOnFocus: false,
+			revalidateOnReconnect: false,
+		},
+	);
 
-  console.log({ query, data, error, isLoading });
+	console.log({ query, data, error, isLoading });
 
-  return {
-    cities: data?.cities || [],
-    isError: error,
-    isLoading,
-  };
+	return {
+		cities: data?.cities || [],
+		isError: error,
+		isLoading,
+	};
 }
