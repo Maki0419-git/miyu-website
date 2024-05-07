b820ee1df6ccdca8943d4aa4b9d7a273c24cb1cc
Maki0419-git <yoshinomaki90530@gmai.com>
2024-05-07 16:23:19 +0800

    fix(weather): apply `setTimeZoneToTaipei` in weather card

--- src/app/weather/utils/createWeatherCardList.ts
+++ src/app/weather/utils/createWeatherCardList.ts
@@ -1,72 +1,61 @@
-import dayjs, { Dayjs } from "dayjs";
+import { Dayjs } from "dayjs";
+import dayjs from "../utils/dayjs";
 import { WeatherElement } from "../types";
 import { WEATHER_ICON } from "../components/server/WeatherIcon";
 import { getWeatherType } from "./getWeatherType";
 import { WEATHER_CODE } from "../constant";
 
 type WeatherCardType = {
-  startTime: Dayjs;
-  weatherElement: {
-    Wx: {
-      description: string;
-      code: number;
-      img: JSX.Element;
-    };
-    PoP: number;
-    MinT: number;
-    MaxT: number;
-  };
+	startTime: Dayjs;
+	weatherElement: {
+		Wx: {
+			description: string;
+			code: number;
+			img: JSX.Element;
+		};
+		PoP: number;
+		MinT: number;
+		MaxT: number;
+	};
 };
 
 export default function createWeatherCardList(
-  weatherElement: WeatherElement[],
-  isDayOrNight: "day" | "night"
+	weatherElement: WeatherElement[],
+	isDayOrNight: "day" | "night",
 ): WeatherCardType[] {
-  const weatherCardList: WeatherCardType[] = weatherElement.reduce(
-    (acc, current) => {
-      const { elementName, time } = current;
-      if (elementName === "CI") return acc;
-      time.forEach((time, index) => {
-        if (!acc[index])
-          acc[index] = {
-            startTime: dayjs(time.startTime),
-            weatherElement: {},
-          } as WeatherCardType;
-        switch (elementName) {
-          case "Wx":
-            acc[index].weatherElement.Wx = {
-              description: time.parameter.parameterName,
-              code: Number(time.parameter.parameterValue as string),
-              img: WEATHER_ICON[isDayOrNight][
-                getWeatherType(
-                  WEATHER_CODE,
-                  Number(time.parameter.parameterValue as string)
-                )
-              ],
-            };
-            break;
-          case "PoP":
-            acc[index].weatherElement.PoP = Number(
-              time.parameter.parameterName
-            );
-            break;
-          case "MinT":
-            acc[index].weatherElement.MinT = Number(
-              time.parameter.parameterName
-            );
-            break;
-          case "MaxT":
-            acc[index].weatherElement.MaxT = Number(
-              time.parameter.parameterName
-            );
-            break;
-        }
-      });
+	const weatherCardList: WeatherCardType[] = weatherElement.reduce((acc, current) => {
+		const { elementName, time } = current;
+		if (elementName === "CI") return acc;
+		time.forEach((time, index) => {
+			if (!acc[index])
+				acc[index] = {
+					startTime: dayjs(time.startTime),
+					weatherElement: {},
+				} as WeatherCardType;
+			switch (elementName) {
+				case "Wx":
+					acc[index].weatherElement.Wx = {
+						description: time.parameter.parameterName,
+						code: Number(time.parameter.parameterValue as string),
+						img: WEATHER_ICON[isDayOrNight][
+							getWeatherType(WEATHER_CODE, Number(time.parameter.parameterValue as string))
+						],
+					};
+					break;
+				case "PoP":
+					acc[index].weatherElement.PoP = Number(time.parameter.parameterName);
+					break;
+				case "MinT":
+					acc[index].weatherElement.MinT = Number(time.parameter.parameterName);
+					break;
+				case "MaxT":
+					acc[index].weatherElement.MaxT = Number(time.parameter.parameterName);
+					break;
+			}
+		});
 
-      return acc;
-    },
-    [] as WeatherCardType[]
-  );
+		return acc;
+	}, [] as WeatherCardType[]);
 
-  return weatherCardList;
+	return weatherCardList;
 }
