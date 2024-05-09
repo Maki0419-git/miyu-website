d767f889739012448a4a339860aab8cce7c81f6b
Maki0419-git <yoshinomaki90530@gmai.com>
2024-05-09 14:42:32 +0800

    fix(sentry): remove unnecessary console

--- src/app/weather/error.tsx
+++ src/app/weather/error.tsx
@@ -5,7 +5,6 @@ import { useEffect } from "react"
 export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
 	useEffect(() => {
 		// Log the error to an error reporting service
-		console.error(error)
 		Sentry.captureException(error)
 	}, [error])
 
