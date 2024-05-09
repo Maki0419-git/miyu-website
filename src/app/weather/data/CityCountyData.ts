012b316d133a457ce761f62449fe833d29a0d921
Maki0419-git <yoshinomaki90530@gmai.com>
2024-05-09 23:12:40 +0800

    feat(weather): add english name for each city

--- src/app/weather/data/CityCountyData.ts
+++ src/app/weather/data/CityCountyData.ts
@@ -1,94 +1,113 @@
-type CityCounty = {
+export type CityCounty = {
 	cityName: string
 	stationName: string
+	englishName: string
 }
 
 export const CityCountyData: CityCounty[] = [
 	{
 		cityName: "臺北市",
 		stationName: "臺北",
+		englishName: "Taipei",
 	},
 	{
 		cityName: "新北市",
 		stationName: "新北",
+		englishName: "NewTaipei",
 	},
 	{
 		cityName: "宜蘭縣",
 		stationName: "宜蘭",
+		englishName: "Yilan",
 	},
 	{
 		cityName: "基隆市",
 		stationName: "基隆",
+		englishName: "Keelung",
 	},
 	{
 		cityName: "桃園市",
 		stationName: "新屋",
+		englishName: "Taoyuan",
 	},
 	{
 		cityName: "新竹市",
 		stationName: "新竹",
+		englishName: "Hsinchu",
 	},
 	{
 		cityName: "新竹縣",
 		stationName: "新竹",
+		englishName: "Hsinchu",
 	},
-
 	{
 		cityName: "臺中市",
 		stationName: "臺中",
+		englishName: "Taichung",
 	},
 	{
 		cityName: "彰化縣",
 		stationName: "田中",
+		englishName: "Changhua",
 	},
-	// no data
 	{
 		cityName: "雲林縣",
 		stationName: "古坑",
+		englishName: "Yunlin",
 	},
-
 	{
 		cityName: "嘉義市",
 		stationName: "嘉義",
+		englishName: "Chiayi",
 	},
 	{
 		cityName: "嘉義縣",
 		stationName: "阿里山",
+		englishName: "Chiayi",
 	},
 	{
 		cityName: "南投縣",
 		stationName: "日月潭",
+		englishName: "Nantou",
 	},
 	{
 		cityName: "臺南市",
 		stationName: "臺南",
+		englishName: "Tainan",
 	},
 	{
 		cityName: "高雄市",
 		stationName: "高雄",
+		englishName: "Kaohsiung",
 	},
 	{
 		cityName: "屏東縣",
 		stationName: "恆春",
+		englishName: "Pingtung",
 	},
 	{
 		cityName: "花蓮縣",
 		stationName: "花蓮",
+		englishName: "Hualien",
 	},
 	{
 		cityName: "臺東縣",
 		stationName: "臺東",
+		englishName: "Taitung",
 	},
 	{
 		cityName: "連江縣",
 		stationName: "馬祖",
+		englishName: "Lienchiang",
 	},
 	{
 		cityName: "澎湖縣",
 		stationName: "澎湖",
+		englishName: "Penghu",
 	},
 	{
 		cityName: "金門縣",
 		stationName: "金門",
+		englishName: "Kinmen",
 	},
 ]
