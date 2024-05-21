f490e62e851fb9a453345260e62e8c416735f81f
Maki0419-git <yoshinomaki90530@gmai.com>
2024-05-21 15:52:34 +0800

    refactor(utils): extends Error as CustomError to throw error with status code

--- src/app/(insert-ad-page)/weather/utils/getStationInfo.ts
+++ src/app/(insert-ad-page)/weather/utils/getStationInfo.ts
@@ -1,8 +1,9 @@
+import { CustomError } from "@/utils/errorHandler"
 import { CityCounty, CityCountyData } from "../data/CityCountyData"
 
 export function getCityInfo(city: string, key: keyof CityCounty) {
 	for (const data of CityCountyData) {
 		if (data.cityName === city) return data[key]
 	}
-	throw new Error(`City ${city} is not available.`)
+	throw new CustomError(400, `City ${city} is not available.`)
 }
--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -2,17 +2,27 @@ import { UnsplashAPIAlias, WeatherApiAlias } from "@/app/(insert-ad-page)/weathe
 
 type apiAlias = WeatherApiAlias | UnsplashAPIAlias
 
+export class CustomError extends Error {
+	constructor(
+		public statusCode: number,
+		public message: string,
+	) {
+		super(message)
+		this.name = "CustomError"
+	}
+}
+
 export default function errorHandler(apiAlias: apiAlias, status: Response["status"]) {
 	switch (status) {
 		case 401:
-			throw new Error(`${apiAlias} Unauthorized`)
+			throw new CustomError(401, `${apiAlias} Unauthorized`)
 		case 403:
-			throw new Error(`${apiAlias} Forbidden`)
+			throw new CustomError(403, `${apiAlias} Forbidden`)
 		case 404:
-			throw new Error(`${apiAlias} Not Found`)
+			throw new CustomError(404, `${apiAlias} Not Found`)
 		case 500:
-			throw new Error(`${apiAlias} Internal Server Error`)
+			throw new CustomError(500, `${apiAlias} Internal Server Error`)
 		default:
-			throw new Error(`${apiAlias} Something went wrong`)
+			throw new CustomError(status, `${apiAlias} Something went wrong`)
 	}
 }
