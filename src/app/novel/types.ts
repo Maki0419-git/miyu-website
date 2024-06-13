d3d2976486ef1c3d8a21aee5c9955ac0da671719
Maki0419-git <yoshinomaki90530@gmai.com>
2024-06-13 18:06:25 +0800

    feat(api): add new api for db query and  types

---
+++ src/app/novel/types.ts
@@ -0,0 +1,28 @@
+export interface Novel {
+	id: number
+	book_name: string
+	description: string
+	create_date: Date
+	author: string
+}
+
+export interface ChapterPreview {
+	id: number
+	title: string
+	novel_id?: number
+	image_file?: string
+	image_url?: string
+}
+
+export interface Chapter extends ChapterPreview {
+	content: string
+}
+
+export interface ChapterAPIResponse {
+	chapter: Chapter
+}
+
+export interface NovelAPIResponse {
+	novel: Novel
+	chapters: ChapterPreview[]
+}
