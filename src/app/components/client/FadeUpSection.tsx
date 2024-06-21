03f4fcbf21cfaa304f9d3e8f5a7b917fba3803e3
Maki0419-git <yoshinomaki90530@gmai.com>
2024-06-21 16:50:31 +0800

    feat(home): let fadeup section can be customized

--- src/app/components/client/FadeUpSection.tsx
+++ src/app/components/client/FadeUpSection.tsx
@@ -1,11 +1,18 @@
 "use client"
 import { useInView, animated } from "@react-spring/web"
-export function FadeUpSection({ children }: { children: React.ReactNode }) {
+
+export function FadeUpSection({
+	children,
+	customizeStyles,
+}: {
+	children: React.ReactNode
+	customizeStyles?: React.CSSProperties
+}) {
 	const [ref, springs] = useInView(
 		() => ({
 			from: {
 				opacity: 0,
-				y: 100,
+				y: 200,
 			},
 			to: {
 				opacity: 1,
@@ -13,13 +20,13 @@ export function FadeUpSection({ children }: { children: React.ReactNode }) {
 			},
 		}),
 		{
-			amount: 0.2,
+			rootMargin: "10px",
 			once: true,
 		},
 	)
 
 	return (
-		<animated.section ref={ref} style={springs}>
+		<animated.section ref={ref} style={{ ...springs, ...customizeStyles }}>
 			{children}
 		</animated.section>
 	)
