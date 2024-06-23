e8b9e7905319c1461b4568dbf3e21816b19be0f6
Maki0419-git <yoshinomaki90530@gmai.com>
2024-06-23 17:13:08 +0800

    feat(home): implement AnimeImageCarousel with lazy load

--- src/app/components/server/RecommendAnime.tsx
+++ src/app/components/server/RecommendAnime.tsx
@@ -1,36 +1,8 @@
-import { firebaseStorage } from "@/libs/firebase"
-import { neon } from "@neondatabase/serverless"
-import { getDownloadURL, ref } from "firebase/storage"
-import { ImageCarousel, ImageData } from "../client"
-
-type Anime = {
-	id: number
-	title: string
-	description: string
-}
-
-async function getRecommendAnime() {
-	try {
-		const sql = neon(process.env.DATABASE_URL || "")
-		const recommendAnime = (await sql`select * from anime;`) as Anime[]
-		const generateAnimeWithImgInfo = async (recommendAnime: Anime[]) => {
-			const promises = recommendAnime.map(async (anime) => {
-				const src = await getDownloadURL(ref(firebaseStorage, `anime/${anime.id}.jpeg` || ""))
-				return { src, alt: anime.title, ...anime }
-			})
-			const urls = await Promise.all(promises)
-			return urls
-		}
-		const animeWithImgInfo: ImageData[] = await generateAnimeWithImgInfo(recommendAnime)
-
-		return { animeWithImgInfo }
-	} catch (e) {
-		throw e
-	}
-}
+import { getRecommendAnime } from "@/app/utils/anime/getRecommendAnime"
+import { AnimeImageCarousel } from "../client/AnimeImageCarousel"
 
 export async function RecommendAnime() {
 	const { animeWithImgInfo } = await getRecommendAnime()
 
-	return <ImageCarousel data={animeWithImgInfo} />
+	return <AnimeImageCarousel data={animeWithImgInfo} />
 }
