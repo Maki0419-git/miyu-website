7b46ef1fba2f7f12d0763d1b5116233a6de0aadd
Maki0419-git <yoshinomaki90530@gmai.com>
2024-07-10 23:34:27 +0800

    refactor(AnimeImageCarousel): remove unnecessary css

--- src/app/components/client/AnimeImageCarousel.tsx
+++ src/app/components/client/AnimeImageCarousel.tsx
@@ -43,7 +43,6 @@ const Mask = styled("div")({
 	height: "100%",
 	background: "linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0,0,0,0.3) 80%, rgba(0, 0, 0, 0.9) 100%)",
 	borderRadius: "8px",
-	border: "1px solid #FF9F1C",
 
 	"&:hover": {
 		[`${BottomSection}`]: {
