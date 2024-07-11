f00087d979d8c50455d9e92ddcda7262d86535dc
Maki0419-git <yoshinomaki90530@gmai.com>
2024-07-11 17:56:47 +0800

    feat(novel): adjust list layout

--- src/app/(insert-ad-page)/novel/[chapter]/client/Article.tsx
+++ src/app/(insert-ad-page)/novel/[chapter]/client/Article.tsx
@@ -8,6 +8,12 @@ const Container = styled("article")({
 	flex: 7,
 	margin: "20px",
 	lineHeight: "2",
+	border: "1px solid #e2dfdf",
+	borderRadius: "8px",
+	boxShadow: "0 0 10px rgba(0,0,0,0.3)",
+	padding: "20px",
+	maxHeight: "calc(100vh - 150px)",
+	overflowY: "scroll",
 	p: {
 		margin: "20px 0",
 	},
